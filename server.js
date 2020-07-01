b3b59f5f70b2a2b2863669d3d048df8bf18a9dd3
JoanAndoni <mcr_joan@hotmail.com>
2020-06-30 20:41:28 -0500

    redirections

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('*', (req, res) => {
